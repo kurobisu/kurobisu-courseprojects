8ae6f32adae1895aaf644b30381596f3caa6f81c
kurobisu <kurobisu.net@gmail.com>
2023-05-28 20:06:58 -0300

    fixing erros on js pt 1

--- pokedex-2/js/script.js
+++ pokedex-2/js/script.js
@@ -6,20 +6,27 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
 
 const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-return data
+    const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+
+    if (APIResponse.status = 200){
+        const data = await APIResponse.json();
+        return data
+    }
 }
 
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value = '';
+    }
 }
 
 form.addEventListener('submit', (event) => {
-
     event.preventDefault();
+    renderPokemon(input.value);
 
 });
